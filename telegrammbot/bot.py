bfed0c69a6959f52accb8b1cacc647f7904a1197
Anastasiya B <gavrilovaanastasya@yandex.ru>
2023-01-09 14:48:06 +0300

    telegrambotstart

---
+++ telegrammbot/bot.py
@@ -0,0 +1,7 @@
+import telebot
+bot = telebot.Telebot("5901080060:AAGwl8wMR8lHsv6JSRZYrIXN21-WKGuC1lY")
+@bot.message_handler(commands=["start"])
+def start (msg):
+    print(msg)
+    bot.send_message(chat_id = msg.chat.id, text = "Это игра Крестики нолики, добро пожаловать!")
+bot.polling()    
